a81139fff79cab6fa2b8f69fec1af888981d6d15
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-06-07 20:31:30 -0500

    se agrego capa de servicios

--- src/com/company/entity/PartidoEntity.java
+++ src/com/company/entity/PartidoEntity.java
@@ -1,4 +1,27 @@
 package com.company.entity;
 
+import java.util.List;
+
 public class PartidoEntity {
+
+    private List<EquipoEntity> equipo;
+    private double puntuacion;
+
+    public List <EquipoEntity> getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(List <EquipoEntity> equipo) {
+        this.equipo = equipo;
+    }
+
+    public double getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(double puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+
 }
